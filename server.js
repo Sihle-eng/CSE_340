0a80ecbba5d20ba0853a25d2f8dbad5810308062
Sihle-eng <smchobokazi@byupathway.edu>
2026-03-18 18:44:18 +0200

    getting there

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
 
 app.set("view engine", "ejs")
 app.use(expressLayouts)
