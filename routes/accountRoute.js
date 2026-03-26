6b2f9418bed1ffdc679e6378595fb3529dda3eae
Sihle-eng <smchobokazi@byupathway.edu>
2026-03-26 21:58:36 +0200

    done

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities")
 const accController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation")
 
 //route to build the loign view
 router.get("/login", accController.buildLogin)
@@ -10,9 +11,13 @@ router.get("/login", accController.buildLogin)
 //route to build the registration view
 router.get("/register", accController.buildRegister)
 
-//route to process account registration 
-router.post("/register", utilities.handleErrors(accController.registerAccount))
-
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accController.registerAccount)
+)
 //route to process account login
 router.post("/login", utilities.handleErrors(accController.loginAccount))
 
